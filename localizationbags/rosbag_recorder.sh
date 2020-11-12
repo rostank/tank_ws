7e64ab345fa933fabb2651ca1ea17b3ca7bb883d
rostank <fskaffe@gmail.com>
2020-11-12 15:10:04 +0100

    Added csv of localtest 2

--- localizationbags/rosbag_recorder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Run as ./rosbag_recorder file_to_use localization_settings
-rosbag record -o $1_$2.bag  -b 1024 --lz4 /odometry/filtered /odometry/filtered_map /odometry/gps __name:=my_bag &
-rosbag play -r 1 ~/tank_ws/sensorbags/$1.bag
-rosnode kill /my_bag
