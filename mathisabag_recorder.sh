a3f0e080a6e1489a566d302324217719d6fa0524
Tank <fskaffe@gmail.com>
2021-01-27 15:45:02 +0100

    final commit

--- mathisabag_recorder.sh
+++ mathisabag_recorder.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rosbag record -b 1024 --lz4 /belt/odom /cmd_vel \
-/imu/data /imu/magnetometer /lwheel \
-/lwheel_vtarget /rwheel /rwheel_vtarget /tf /tf_static /twist_au \
-/twist_joy \
+rosbag record -b 1024 --lz4  /cmd_vel \
+/imu/magnetometer  \
+ /tf /tf_static /twist_au \
+/twist_joy /move_base/TrajectoryPlannerROS/local_plan \
+/move_base/GlobalPlanner/plan \
 /zed2/zed_node/odom /zed2/zed_node/path_map /zed2/zed_node/path_odom \
 /zed2/zed_node/pose /zed2/zed_node/pose_with_covariance \
 /odometry/filtered /odometry/filtered_map
